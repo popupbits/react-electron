ec3e808cd0dd3affe9b69368d2c796de375caaa4
Damodar Lohani <dlohani48@gmail.com>
2018-01-05 13:53:35 +0545

    yarn everywhere

--- electron-wait-react.js
+++ electron-wait-react.js
@@ -12,7 +12,7 @@ const tryConnection = () => client.connect({port: port}, () => {
             console.log('starting electron');
             startedElectron = true;
             const exec = require('child_process').exec;
-            exec('npm run electron');
+            exec('yarn run electron');
         }
     }
 );
